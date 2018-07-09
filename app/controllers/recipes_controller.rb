99b0a6d9d4a49b8ea57ec4522364e4c18aa2382c
nichia <nichia@gmail.com>
2018-07-09 11:37:40 -0400

    Add create new recipe route/view

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -19,11 +19,49 @@ class RecipesController < ApplicationController
   # GET /recipes/new - new action - displays create recipe form
   get '/recipes/new' do
     if Helpers.logged_in?(session)
+      @categories = Category.all.sort_by do |category|
+        category.name
+      end
+
       erb :'/recipes/new'
     else
       flash[:message] = "You must be logged in to add a recipe."
       redirect :"/login"
     end
+  end
+
+  # POST /recipes - create action - create a new recipe
+  post '/recipes' do
+    binding.pry
+
+    if params[:recipe][:name].empty?
+      flash[:message] = "Your recipe name cannot be left blank."
+      redirect :"/recipes/new"
+    else
+      recipe = Recipe.create(params[:recipe])
+      recipe.user_id = session[:user_id]
+
+      # new category
+      if !params[:category][:name].empty?
+        if Category.find_by(name: params[:category][:name])
+          flash[:message] = "Category already exists, please select from checklist."
+        else
+          recipe.categories << Category.create(name: params[:category][:name])
+        end
+      end
+
+
+      recipe.save
+
+      flash[:message] = "Successfully added your recipe."
+      redirect :"/recipes/#{recipe.slug}"
+    end
+  end
+
+  get '/recipes/:slug' do
+    @recipe = Recipe.find_by_slug(params[:slug])
+
+    erb :'/recipes/show'
   end
 
 end
